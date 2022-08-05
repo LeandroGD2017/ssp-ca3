be13d9e40816a5aafb155c15137d291acd8398e6
Leandro <leoguimaraesd@gmail.com>
2022-08-05 02:15:51 +0100

    added delete function to server

--- app.js
+++ app.js
@@ -47,6 +47,33 @@ router.get(
   }
 );
 
+//delete job
+router.get("/title/:title/company/:company", (req, res) => {
+  console.log(req.params.title, req.params.company);
+
+  const fs = require("fs");
+  let rawdata = fs.readFileSync("jobs.json");
+  let jobs = JSON.parse(rawdata);
+
+  for (i = 0; i < jobs.list.length; i++) {
+    if (
+      jobs.list[i].title == req.params.title &&
+      jobs.list[i].company == req.params.company
+    ) {
+      jobs.list.splice(i, 1);
+    }
+  }
+
+  const jsonString = JSON.stringify(jobs, null, 2);
+  fs.writeFile("./jobs.json", jsonString, (err) => {
+    if (err) {
+      console.log("Error writing file", err);
+    } else {
+      console.log("Successfully wrote file");
+    }
+  });
+});
+
 //add the router
 app.use("/", router);
 app.listen(process.env.port || 8000);
